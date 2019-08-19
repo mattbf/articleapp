103aa13855ca3e6edb733ccabd14da40b7532a72
mattbf <matthewbf8@gmail.com>
2019-08-19 11:41:21 -0600

    trying to add ui styling components to editor

--- src/Components/ArticleEditor.js
+++ src/Components/ArticleEditor.js
@@ -1,18 +1,200 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import ReactDOM from 'react-dom';
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, RichUtils, getDefaultKeyBinding} from 'draft-js';
+import {
+  Pane,
+  IconButton,
+  Text,
+  Heading,
+  Avatar,
+  TextInput
+} from 'evergreen-ui'
+
+
 
 function ArticleEditor() {
   const [editorState, setEditorState] = React.useState(
     EditorState.createEmpty()
   );
+  const buttonSelected = false
+  const editorRef = useRef(null);
+  const onChange = (editorState) => setEditorState({editorState});
+  const focus = () => editorRef.current.editor.focus();
+  function handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      setEditorState(newState);
+      return true;
+    }
+    return false;
+  }
+
+  function mapKeyToEditorCommand(e) {
+    if (e.keyCode === 9 /* TAB */) {
+      const newEditorState = RichUtils.onTab(
+        e,
+        editorState,
+        4, /* maxDepth */
+      );
+      if (newEditorState !== editorState) {
+        setEditorState(newEditorState);
+      }
+      return;
+    }
+    return getDefaultKeyBinding(e);
+  }
+
+  function toggleBlockType(blockType) {
+    onChange(
+      RichUtils.toggleBlockType(
+        editorState,
+        blockType
+      )
+    );
+  }
+
+  function toggleInlineStyle(inlineStyle) {
+    onChange(
+      RichUtils.toggleInlineStyle(
+        editorState,
+        inlineStyle
+      )
+    );
+  }
+
+  // Custom overrides for "code" style.
+      const styleMap = {
+        CODE: {
+          backgroundColor: 'rgba(0, 0, 0, 0.05)',
+          fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
+          fontSize: 16,
+          padding: 2,
+        },
+      };
+      function getBlockStyle(block) {
+        switch (block.getType()) {
+          case 'blockquote': return 'RichEditor-blockquote';
+          default: return null;
+        }
+      }
+      function StyleButton(props) {
+        const onToggle = (e) => {
+          e.preventDefault();
+          props.onToggle(props.style);
+        };
+        // constructor() {
+        //   super();
+        //   this.onToggle = (e) => {
+        //     e.preventDefault();
+        //     this.props.onToggle(this.props.style);
+        //   };
+        // }
+        // render() {
+        //   let className = 'RichEditor-styleButton';
+        //   if (props.active) {
+        //     className += ' RichEditor-activeButton';
+        //   }
+          return (
+            <span onMouseDown={onToggle}>
+              {props.label}
+            </span>
+          );
+        }
+      const BLOCK_TYPES = [
+        {label: 'H1', style: 'header-one'},
+        {label: 'H2', style: 'header-two'},
+        {label: 'H3', style: 'header-three'},
+        {label: 'H4', style: 'header-four'},
+        {label: 'H5', style: 'header-five'},
+        {label: 'H6', style: 'header-six'},
+        {label: 'Blockquote', style: 'blockquote'},
+        {label: 'UL', style: 'unordered-list-item'},
+        {label: 'OL', style: 'ordered-list-item'},
+        {label: 'Code Block', style: 'code-block'},
+      ];
+      const BlockStyleControls = (props) => {
+        const {editorState} = props;
+        const selection = editorState.getSelection();
+        const blockType = editorState
+          .getCurrentContent()
+          .getBlockForKey(selection.getStartKey())
+          .getType();
+        return (
+          <div className="RichEditor-controls">
+            {BLOCK_TYPES.map((type) =>
+              <StyleButton
+                key={type.label}
+                active={type.style === blockType}
+                label={type.label}
+                onToggle={props.onToggle}
+                style={type.style}
+              />
+            )}
+          </div>
+        );
+      };
+      var INLINE_STYLES = [
+        {label: 'Bold', style: 'BOLD'},
+        {label: 'Italic', style: 'ITALIC'},
+        {label: 'Underline', style: 'UNDERLINE'},
+        {label: 'Monospace', style: 'CODE'},
+      ];
+      const InlineStyleControls = (props) => {
+        const currentStyle = props.editorState.getCurrentInlineStyle();
+
+        return (
+          <div className="RichEditor-controls">
+            {INLINE_STYLES.map((type) =>
+              <StyleButton
+                key={type.label}
+                active={currentStyle.has(type.style)}
+                label={type.label}
+                onToggle={props.onToggle}
+                style={type.style}
+              />
+            )}
+          </div>
+        );
+      };
 
   return (
-    <Editor
-      editorState={editorState}
-      onChange={setEditorState}
-    />
+    <div className="RichEditor-root">
+      <BlockStyleControls
+        editorState={editorState}
+        onToggle={toggleBlockType}
+      />
+      <InlineStyleControls
+        editorState={editorState}
+        onToggle={toggleInlineStyle}
+      />
+      <div onClick={focus}>
+        <Editor
+          blockStyleFn={getBlockStyle}
+          customStyleMap={styleMap}
+          editorState={editorState}
+          handleKeyCommand={handleKeyCommand}
+          keyBindingFn={mapKeyToEditorCommand}
+          onChange={onChange}
+          placeholder="Tell a story..."
+          ref={editorRef}
+          spellCheck={true}
+        />
+      </div>
+    </div>
   );
-}
+  }
 
 export default ArticleEditor
+
+// <div>
+//   <Editor
+//     editorState={editorState}
+//     onChange={setEditorState}
+//   />
+//   <IconButton
+//       marginBottom={16}
+//       appearance={buttonSelected ? null : 'minimal'}
+//       height={24}
+//       icon="cross"
+//   />
+// </div>
