7871ac76647c5aefd450dc6b1948907276d90e38
mattbf <matthewbf8@gmail.com>
2019-08-21 11:23:02 -0600

    displayed data on UI

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -8,25 +8,44 @@ function Todo() {
     axios.get('http://localhost:4000/todos/')
         .then(response => {
             setTodos({ todos: response.data });
-            console.log(response.data)
+            console.log(todos)
         })
         .catch(function (error){
             console.log(error);
         })
   }, []); //  Only re-run the effect if count changes
 
+  console.log(todos)
   return(
     <div>
       Todos
+      {todos.todos ?
+        <div>
+          {todos.todos.map((todo, index) =>
+            <div key={todo._id}>
+              "description:"
+              {todo.todo_description}
+              {todo.todo_priority}
+            </div>
+          )}
+        </div>
+        :
+        "loading"
+      }
     </div>
   )
 }
 
 export default Todo
 
-
-// {todos.map((index, todo) =>
+// {todos ?
 //   <div>
-//     {todo.}
+//     {todos.map((index, todo) =>
+//       <div>
+//         {todo.todo_description}
+//       </div>
+//     )}
 //   </div>
-// )}
+//   :
+//   "loading"
+// }
