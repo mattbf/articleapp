a9bccb9de827a84ea8a67dac67f10333a0da7a56
mattbf <matthewbf8@gmail.com>
2019-08-23 07:15:47 -0600

    Added fetch state to GET request

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,6 +1,11 @@
 import React, {useEffect, useState} from 'react'
 import ArticleBlock from './ArticleBlock'
 import axios from 'axios'
+import {
+  Heading,
+  Pane,
+  Spinner
+} from 'evergreen-ui'
 
 const data = [
   {
@@ -63,23 +68,61 @@ const data = [
 function Feed() {
   const [feed, setFeed] = useState([])
   const articleCount = feed.length
+  const [fetch, setFetch] = useState({
+    isLoading: false,
+    isError: false,
+    error: null
+  })
 
   useEffect(() => {
+    setFetch({
+      isLoading: true,
+      isError: false,
+      error: null
+    })
     axios.get('http://localhost:4000/articles/')
         .then(response => {
             setFeed(response.data);
-            //console.log(articles)
+            setFetch({
+              isLoading: false,
+              isError: false,
+              error: null
+            })
         })
         .catch(function (error){
+            setFetch({
+              isLoading: false,
+              isError: true,
+              error: error
+            })
             console.log(error);
         })
-  })
+  }, [])
 
   return (
     <div>
-      {feed.map((article, index) =>
-        <ArticleBlock key={article._id} article={article} index={index + 1} number={articleCount - index}/>
-      )}
+      {fetch.isLoading ?
+        <Pane display="flex" alignItems="center" justifyContent="center" height={400}>
+          <Spinner />
+        </Pane>
+        :
+        fetch.isError ?
+          <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginLeft: 'auto', marginRight: 'auto', minHeight: '400px'}}>
+            <Heading marginBottom={10} size={700}>Oops.. Looks like Something went wrong!</Heading>
+            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', }}>
+              <Heading size={500}>
+              {fetch.error.message}.. Try refreshing the page
+              </Heading>
+            </div>
+          </div>
+          :
+          <div>
+            {feed.map((article, index) =>
+              <ArticleBlock key={article._id} article={article} index={index + 1} number={articleCount - index}/>
+            )}
+          </div>
+      }
+
     </div>
   )
 }
